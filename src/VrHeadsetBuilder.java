f13c0e8a505594417f2eb3976bea5fa3e92f7752
shyng <252276@astanait.edu.kz>
2026-09-13 22:31:24 +0500

    Defined VrHeadsetBuilder interface

--- src/VrHeadsetBuilder.java
+++ src/VrHeadsetBuilder.java
@@ -1,2 +1,8 @@
-public class VrHeadsetBuilder {
+public interface VrHeadsetBuilder {
+    VrHeadsetBuilder setStrap(StrapType strap);
+    VrHeadsetBuilder setFaceCover(FacialInterface cover);
+    VrHeadsetBuilder setControllers(ControllerType controllers);
+    VrHeadsetBuilder addPcLinkCable(boolean hasCable);
+    VrHeadsetBuilder enableEyeTracking(boolean hasTracking);
+    VrHeadset build();
 }
